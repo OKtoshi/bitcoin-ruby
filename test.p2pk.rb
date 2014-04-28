9bcb975d198a656140c5e4cf0691d8e8c3cb9358
Scott Li <scottli0101@gmail.com>
2014-04-28 12:24:51 -0700

    nice tests

--- test.p2pk.rb
+++ test.p2pk.rb
@@ -3,38 +3,28 @@ Bitcoin.network = :testnet3
 
 include Bitcoin::Builder
 
-privs = [
-  "f457f5c3ce39c5041935f047525e3d12d74a7a4ba7321393bf8167a465051b99",
-]
-
-keys = privs.map do |priv|
-  Bitcoin::Key.new(priv, nil, false)
-end
-
-keys[0].address
+priv = "f457f5c3ce39c5041935f047525e3d12d74a7a4ba7321393bf8167a465051b99"
+key = Bitcoin::Key.new(priv, nil, false)
+p key.addr
 
 unspent = {
-  confirmations: 1,
-  blockHeight: 227071,
-  txHash: 'cef3e503886b597bf67cea46c21f22f9ef8e238dad6f595dcd2a42c1c2eb61c9',
+  confirmations: 136,
+  blockHeight: 226938,
+  txHash: "7212ed1a1f095c3c2cdf0095268066fc2d731039dfd7dcded36d4f2b48252f0c",
   index: 0,
-  scriptPubKey: 'a9148a8b86cab5d35efea59c6fa9b416fbea07c7452f87',
-  type: 'scripthash',
-  value: 100000,
-  hash160: '8a8b86cab5d35efea59c6fa9b416fbea07c7452f',
-  address: '2N5snPEHUgsw5FjypjZ5zR8DQKqotmU5YAR' }
+  scriptPubKey: "76a914652c453e3f8768d6d6e1f2985cb8939db91a4e0588ac",
+  type: "pubkeyhash",
+  value: 9500,
+  hash160: "652c453e3f8768d6d6e1f2985cb8939db91a4e05",
+  address: "mpjuaPusdVC5cKvVYCFX94bJX1SNUY8EJo"
+}
 
-pubkeys = keys.map { |k| k.pub }.join(" ")
-redeem_script = Bitcoin::Script.from_string("2 #{pubkeys} 3 OP_CHECKMULTISIG")
-
-binding.pry
-
-new_tx = build_tx({p2sh_multisig: true}) do |t|
+new_tx = build_tx do |t|
   t.input do |i|
     i.prev_out unspent[:txHash]
     i.prev_out_index unspent[:index]
-    i.prev_out_script redeem_script.raw
-    i.signature_key keys[0..1]
+    i.prev_out_script [unspent[:scriptPubKey]].pack("H*")
+    i.signature_key key
   end
 
   t.output do |o|
--- test.p2sh.rb
+++ test.p2sh.rb
@@ -1,45 +1,27 @@
 require 'pry'
+require 'helloblock-lite'
 Bitcoin.network = :testnet3
 
 include Bitcoin::Builder
 
-privs = [
-  "f457f5c3ce39c5041935f047525e3d12d74a7a4ba7321393bf8167a465051b99",
-  "7218d2410ccf203b3d99e1448ddcd0d4f7a0c09d16f8f0611d0104e94b88629d",
-  "910e6a8ef179f1ee0187b5eebb52dbb06958d34d17a2e350036db280fd7c4eea",
-]
+priv = "f457f5c3ce39c5041935f047525e3d12d74a7a4ba7321393bf8167a465051b99"
+key = Bitcoin::Key.new(priv, nil, false)
 
-keys = privs.map do |priv|
-  Bitcoin::Key.new(priv, nil, false)
-end
-
-unspent = {
-  confirmations: 1,
-  blockHeight: 227071,
-  txHash: 'cef3e503886b597bf67cea46c21f22f9ef8e238dad6f595dcd2a42c1c2eb61c9',
-  index: 0,
-  scriptPubKey: 'a9148a8b86cab5d35efea59c6fa9b416fbea07c7452f87',
-  type: 'scripthash',
-  value: 100000,
-  hash160: '8a8b86cab5d35efea59c6fa9b416fbea07c7452f',
-  address: '2N5snPEHUgsw5FjypjZ5zR8DQKqotmU5YAR' }
-
-pubkeys = keys.map { |k| k.pub }.join(" ")
-redeem_script = Bitcoin::Script.from_string("2 #{pubkeys} 3 OP_CHECKMULTISIG")
+unspent = HelloBlock::Address.get_unspents(key.addr)[0]
 
 binding.pry
 
-new_tx = build_tx({p2sh_multisig: true}) do |t|
+new_tx = build_tx do |t|
   t.input do |i|
-    i.prev_out unspent[:txHash]
-    i.prev_out_index unspent[:index]
-    i.prev_out_script redeem_script.raw
-    i.signature_key keys[0..1]
+    i.prev_out unspent["txHash"]
+    i.prev_out_index unspent["index"]
+    i.prev_out_script [unspent["scriptPubKey"]].pack("H*")
+    i.signature_key key
   end
 
   t.output do |o|
-    o.value unspent[:value]
-    o.script { |s| s.recipient("mvaRDyLUeF4CP7Lu9umbU3FxehyC5nUz3L") }
+    o.value unspent["value"]
+    o.script { |s| s.recipient("2N5snPEHUgsw5FjypjZ5zR8DQKqotmU5YAR") }
   end
 end
 
